ebbbabe68caeb6a60f6a2876d0b9bbd2a815e9f1
Emma <jsr4922@rit.edu>
2026-09-02 14:32:49 -0400

    added page2

--- src/responses.js
+++ src/responses.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
+const page2 = fs.readFileSync(`${__dirname}/../client/client2.html`);
 
 const getIndex = (req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
@@ -8,4 +9,10 @@ const getIndex = (req, res) => {
     res.end();
 };
 
-module.exports = { getIndex };
+const getPage2 = (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(page2);
+    res.end();
+}
+
+module.exports = { getIndex, getPage2 };
--- src/server.js
+++ src/server.js
@@ -5,7 +5,13 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const onRequest = (req, res) => {
     console.log(req.url);
-    responses.getIndex(req, res);
+
+    if (req.url === '/page2') {
+        responses.getPage2(req, res);
+    }
+    else {
+        responses.getIndex(req, res);
+    }
 };
 
 http.createServer(onRequest).listen(port, () => {
