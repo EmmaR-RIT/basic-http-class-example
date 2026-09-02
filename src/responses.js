9d9997bba5179ebd565723bf39a1e78165d3024b
Emma <jsr4922@rit.edu>
2026-09-02 14:36:58 -0400

    refactored page getters

--- src/responses.js
+++ src/responses.js
@@ -3,16 +3,18 @@ const fs = require('fs');
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 const page2 = fs.readFileSync(`${__dirname}/../client/client2.html`);
 
-const getIndex = (req, res) => {
+const sendPage = (req, res, page) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(index);
+    res.write(page);
     res.end();
+}
+
+const getIndex = (req, res) => {
+    sendPage(req, res, index);
 };
 
 const getPage2 = (req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(page2);
-    res.end();
+    sendPage(req, res, page2);
 }
 
 module.exports = { getIndex, getPage2 };
