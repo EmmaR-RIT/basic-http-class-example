31b851bc9520dde1cc75e67a2368391bfb90618d
Emma <jsr4922@rit.edu>
2026-09-04 14:46:36 -0400

    more refactor, css send wrong mime type idk why

--- src/responses.js
+++ src/responses.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 const page2 = fs.readFileSync(`${__dirname}/../client/client2.html`);
-const style = fs.readFileSync(`${__dirname}/../client/style.css`);
+const style = fs.readFileSync(`${__dirname}/../client/styles.css`);
 
 const serveFile = (req, res, content, mime) => {
     res.writeHead(200, { 'Content-Type': mime });
@@ -10,14 +10,12 @@ const serveFile = (req, res, content, mime) => {
     res.end();
 };
 
-const sendPage = (req, res, page) => { serveFile(req, res, page, 'text/html'); };
+const getMessage = (req, res) => serveFile(req, res, 'Hello, World!', 'text/plain');
 
-const getMessage = (req, res) => { serveFile(req, res, 'Hello, World!', 'text/plain'); };
+const getIndex = (req, res) => serveFile(req, res, index, 'text/html');
 
-const getIndex = (req, res) => { sendPage(req, res, index); };
+const getPage2 = (req, res) => serveFile(req, res, page2, 'text/html');
 
-const getPage2 = (req, res) => { sendPage(req, res, page2); };
-
-const getCSS = (req, res) => { serveFile(req, res, style, 'text/css'); };
+const getCSS = (req, res) => serveFile(req, res, style, 'text/css');
 
 module.exports = { getIndex, getPage2, getMessage, getCSS };
