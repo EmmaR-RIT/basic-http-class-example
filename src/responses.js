9d2fa84597ffabe2ec5ca3b7d74e7efc6768ab33
Emma <jsr4922@rit.edu>
2026-09-04 14:37:05 -0400

    refactoring and new css endpoint

--- src/responses.js
+++ src/responses.js
@@ -2,25 +2,22 @@ const fs = require('fs');
 
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 const page2 = fs.readFileSync(`${__dirname}/../client/client2.html`);
+const style = fs.readFileSync(`${__dirname}/../client/style.css`);
 
-const sendPage = (req, res, page) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(page);
+const serveFile = (req, res, content, mime) => {
+    res.writeHead(200, { 'Content-Type': mime });
+    res.write(content);
     res.end();
-}
-
-const getMessage = (req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write('Hello, World!');
-    res.end();
-}
-
-const getIndex = (req, res) => {
-    sendPage(req, res, index);
 };
 
-const getPage2 = (req, res) => {
-    sendPage(req, res, page2);
-}
+const sendPage = (req, res, page) => { serveFile(req, res, page, 'text/html'); };
 
-module.exports = { getIndex, getPage2, getMessage };
+const getMessage = (req, res) => { serveFile(req, res, 'Hello, World!', 'text/plain'); };
+
+const getIndex = (req, res) => { sendPage(req, res, index); };
+
+const getPage2 = (req, res) => { sendPage(req, res, page2); };
+
+const getCSS = (req, res) => { serveFile(req, res, style, 'text/css'); };
+
+module.exports = { getIndex, getPage2, getMessage, getCSS };
--- src/server.js
+++ src/server.js
@@ -6,14 +6,19 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 const onRequest = (req, res) => {
     console.log(req.url);
 
-    if (req.url === '/page2') {
-        responses.getPage2(req, res);
-    }
-    else if (req.url === '/message') {
-        responses.getMessage(req, res);
-    }
-    else {
-        responses.getIndex(req, res);
+    switch (req.url) {
+        case '/page2':
+            responses.getPage2(req, res);
+            break;
+        case '/message':
+            responses.getMessage(req, res);
+            break;
+        case '/style.css':
+            responses.getCSS(req,res);
+            break;
+        default:
+            responses.getIndex(req, res);
+            break;
     }
 };
 
